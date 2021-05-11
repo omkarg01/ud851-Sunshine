daca79ae71721116d47175e82a859caa56226c4c
omkar01 <omkargujja01@gmail.com>
2021-05-11 17:43:49 +0530

    4.2 Exercise Complete

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -13,5 +15,11 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         // TODO (2) Display the weather forecast that was passed from MainActivity
+        TextView tv = findViewById(R.id.display_tv);
+
+        Intent intent = getIntent();
+        String text = intent.getStringExtra(Intent.EXTRA_TEXT);
+        tv.setText(text);
+
     }
 }
